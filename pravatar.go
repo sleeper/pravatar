372d0f5699613ba2961c8b09447c074cfb457f83
Frederick Ros <frederick.ros@gmail.com>
2013-06-03 08:07:04 +0200

    Added command line switches

--- pravatar.go
+++ pravatar.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"github.com/WeGoTogether/pravatar/diskstore"
 	"github.com/gorilla/mux"
@@ -9,8 +10,17 @@ import (
 	"net/http"
 )
 
+const (
+	defaultPort = "3333"
+	defaultHost = ""
+	defaultDir  = "images"
+)
+
 var router = mux.NewRouter()
-var store = diskstore.NewStore("tests")
+var store *diskstore.DiskStore
+var host = defaultHost
+var port = defaultPort
+var dir = defaultDir
 
 func GetAvatarHandler(writer http.ResponseWriter, request *http.Request) {
 	vars := mux.Vars(request)
@@ -28,11 +38,23 @@ func PostAvatarHandler(writer http.ResponseWriter, request *http.Request) {
 }
 
 func init() {
+	flag.StringVar(&port, "port", defaultPort, "port on which to listen")
+	flag.StringVar(&host, "host", defaultHost, "host on which to listen")
+	flag.StringVar(&dir, "dir", defaultDir, "root dir for images")
+
 	router.HandleFunc("/avatar/{hash}", GetAvatarHandler).Methods("GET")
 	router.HandleFunc("/avatar/{hash}", PostAvatarHandler).Methods("POST")
 }
 
 func main() {
+	flag.Parse()
+	var hostAndPort = host + ":" + port
+
+	log.Printf("Listening on %s", hostAndPort)
+	log.Printf("Images root dir is %s", dir)
+
+	store = diskstore.NewStore(dir)
+
 	http.Handle("/", router)
-	http.ListenAndServe(":3333", nil)
+	http.ListenAndServe(hostAndPort, nil)
 }
